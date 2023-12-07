981ee4b5f1353a5fff2bb04eb18e66ef1fe1a9ef
Timofey Denisenko <ftdabcde@gmail.com>
2023-12-07 21:08:08 +0700

    add printers for `Temperature` and `FanPower`
    Signed-off-by: Timofey Denisenko <ftdabcde@gmail.com>

--- src/fan.rs
+++ src/fan.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fmt};
 
 mod pwm;
 
@@ -15,5 +15,45 @@ pub trait Fan {
 impl From<u8> for FanPower {
     fn from(value: u8) -> Self {
         Self(value)
+    }
+}
+
+impl fmt::Display for FanPower {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let precision = f.precision().unwrap_or(1);
+
+        let power = if f.alternate() {
+            let power = self.0;
+            format!("FanPower({power})")
+        } else {
+            let power = (self.0 as f32) / 2.55;
+            format!("{power:.precision$}%")
+        };
+
+        if let Some(width) = f.width() {
+            let to_fill = width.checked_sub(power.len()).unwrap_or(0);
+            let (left, right) = match f.align().unwrap_or(fmt::Alignment::Right) {
+                fmt::Alignment::Right => (to_fill, 0),
+                fmt::Alignment::Center => {
+                    if to_fill % 2 == 1 {
+                        (1 + to_fill / 2, to_fill / 2)
+                    } else {
+                        (to_fill / 2, to_fill / 2)
+                    }
+                }
+                fmt::Alignment::Left => (0, to_fill),
+            };
+
+            let filler = f.fill();
+            let (left, right) = (vec![filler; left], vec![filler; right]);
+            let string: String = left
+                .into_iter()
+                .chain(power.chars())
+                .chain(right.into_iter())
+                .collect();
+            f.write_str(&string)
+        } else {
+            f.write_str(&power)
+        }
     }
 }
--- src/source.rs
+++ src/source.rs
@@ -1,7 +1,7 @@
 mod file;
 mod nvidia;
 
-use std::error::Error;
+use std::{error::Error, fmt};
 
 pub use file::SourceFile;
 pub use nvidia::SourceNvidia;
@@ -23,4 +23,43 @@ impl Temperature {
 /// trait for access source of temperature
 pub trait Source {
     fn value(&self) -> Result<Temperature, Box<dyn Error>>;
+}
+
+impl fmt::Display for Temperature {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let precision = f.precision().unwrap_or(2);
+
+        let temp = if f.alternate() {
+            let value = self.0;
+            format!("Temperature({value})")
+        } else {
+            let value = self.0;
+            format!("{value:.precision$}°C")
+        };
+
+        if let Some(width) = f.width() {
+            let to_fill = width.checked_sub(temp.chars().count()).unwrap_or(0);
+            let (left, right) = match f.align().unwrap_or(fmt::Alignment::Right) {
+                fmt::Alignment::Right => (to_fill, 0),
+                fmt::Alignment::Center => {
+                    if to_fill % 2 == 1 {
+                        (1 + to_fill / 2, to_fill / 2)
+                    } else {
+                        (to_fill / 2, to_fill / 2)
+                    }
+                }
+                fmt::Alignment::Left => (0, to_fill),
+            };
+
+            let filler = f.fill();
+            let string: String = std::iter::repeat(filler)
+                .take(left)
+                .chain(temp.chars())
+                .chain(std::iter::repeat(filler).take(right))
+                .collect();
+            f.write_str(&string)
+        } else {
+            f.write_str(&temp)
+        }
+    }
 }
